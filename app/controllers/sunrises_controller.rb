5fd6c92aef0c3ecd52bb0c0edc5000236ca9f09a
Kyle Petersen <kpete2017@gmail.com>
2020-06-09 13:48:43 -0600

    Added Geocoding

--- app/controllers/sunrises_controller.rb
+++ app/controllers/sunrises_controller.rb
@@ -1,8 +1,8 @@
 class SunrisesController < ApplicationController
-    require 'net/http'
-    require 'open-uri'
+
 
     def index
+        results = Geocoder.search("Paris")
         @default = Sunrise.first
         webrequest = "https://api.sunrise-sunset.org/json?"
         lat = "lat=#{@default.lat}&"
@@ -13,23 +13,24 @@ class SunrisesController < ApplicationController
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         final_result = JSON.parse(response.body)
-        render json: final_result
+        render json: results.first.coordinates
     end
 
     def create
         webrequest = "https://api.sunrise-sunset.org/json?"
-        lat = "lat=#{params[:lat]}&"
-        lng = "lng=#{params[:lng]}&"
+        search_query = Geocoder.search(params[:location])
+        coordinates = search_query.first.coordinates
+        lat = "lat=#{coordinates[0]}&"
+        lng = "lng=#{coordinates[1]}&"
         date = "date=#{params[:date]}"
         full_string = webrequest + lat + lng + date
-        url = full_string
-        uri = URI.parse(url)
+        uri = URI.parse(full_string)
         response = Net::HTTP.get_response(uri)
         final_result = JSON.parse(response.body)
         render json: final_result
         new_sunrise = Sunrise.new
-        new_sunrise.lat = params[:lat]
-        new_sunrise.lng = params[:lng]
+        new_sunrise.lat = coordinates[0]
+        new_sunrise.lng = coordinates[1]
         new_sunrise.date = params[:date]
         new_sunrise.sunrise_time = final_result["results"]["sunrise"]
         new_sunrise.save
